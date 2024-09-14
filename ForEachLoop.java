625d9ff41e967090713dedbbd5ff3bdafb01c8f7
Manjeet Singh <Manjeet.16205@stu.upes.ac.in>
2024-09-15 00:12:53 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,10 @@
+
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int [] marks = {94,45,67,58,74};
+        for( int element : marks) {
+            System.out.println(element);
+        }
+    }
+}
